0366cfffab646dd82fb63e09548ad3f2ec57edf2
Maria Villaseñor <maria.villasenor@siticsoftware.com>
2024-10-19 11:47:45 -0600

    Instalación Tailwind

--- Angular/bases/src/app/app.module.ts
+++ Angular/bases/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,8 @@ import { CounterModule } from './counter/counter.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
 
     // COMPONENTES CUSTOM
     CounterModule
